102ff49c5403f0360b9e90b59252d0e3b2afc3c3
韩潇洋 <122191055+0218Ling@users.noreply.github.com>
2023-11-23 18:18:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,35 +8,35 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx127302970f8fd1ec',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '40b7423834baea4870b95f4fd1d1289e',
+  APP_SECRET: '',
 
-  PROVINCE: '福建',
-  CITY: '南平',
+  PROVINCE: '',
+  CITY: '',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '呆呆',
+      name: '',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o3wKR6Sat7cr3KFpz5VWcNbh1xyA',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	Uf89cHt34yiDP4egz8zL82T_4ZvmT2t4QCwnWZp62wA',
+      useTemplateId: '	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '04-19',
+      horoscopeDate: '',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '呆呆', year: '2003', date: '03-18',
+          type: '*生日', name: '', year: '', date: '',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '呆呆', year: '2003', date: '04-19',
+          type: '生日', name: '', year: '', date: '',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2022', date: '10-04',
+          type: '节日', name: '', year: '', date: '',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -49,13 +49,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'pN8BH6C9OGuO8yO7OzLPmzY3m54ax4zRR_4CzDaa3bI',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o3wKR6Sat7cr3KFpz5VWcNbh1xyA',
+      id: '',
     }
   ],
 
